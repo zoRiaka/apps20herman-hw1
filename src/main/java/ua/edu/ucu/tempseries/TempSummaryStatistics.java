738b1fea7ad9977f941c24b1cff51c7e45e11276
zoriankaH <60688935+zoriankaH@users.noreply.github.com>
2020-11-12 10:14:43 +0200

    Update TempSummaryStatistics.java

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -1,7 +1,8 @@
 package ua.edu.ucu.tempseries;
 
-public class TempSummaryStatistics extends TemperatureSeriesAnalysis {
-    private double avgTemp = this.average();
-
-
+public final class TempSummaryStatistics extends TemperatureSeriesAnalysis {
+    private final double avgTemp = this.average();
+    private final double devTemp = this.deviation();
+    private final double minTemp = this.min();
+    private final double maxTemp = this.max();
 }

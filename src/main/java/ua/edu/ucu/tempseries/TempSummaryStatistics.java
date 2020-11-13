e4f31e43c17c7ee4abf63e3588a00c545e4f6fbb
zoriankaH <60688935+zoriankaH@users.noreply.github.com>
2020-11-13 22:02:51 +0200

    Delete TempSummaryStatistics.java

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++
@@ -1,8 +0,0 @@
-package ua.edu.ucu.tempseries;
-
-public final class TempSummaryStatistics extends TemperatureSeriesAnalysis {
-    private final double avgTemp = this.average();
-    private final double devTemp = this.deviation();
-    private final double minTemp = this.min();
-    private final double maxTemp = this.max();
-}

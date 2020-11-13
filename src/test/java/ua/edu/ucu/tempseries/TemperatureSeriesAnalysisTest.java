6e57ef61450b4038db46988a183b685dcef08d66
zoriankaH <60688935+zoriankaH@users.noreply.github.com>
2020-11-13 22:05:42 +0200

    Update TemperatureSeriesAnalysisTest.java

--- src/test/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysisTest.java
+++ src/test/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysisTest.java
@@ -3,6 +3,9 @@ package ua.edu.ucu.tempseries;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.Ignore;
+
+import java.util.Arrays;
+
 public class TemperatureSeriesAnalysisTest {
 
     @Test
@@ -33,18 +36,28 @@ public class TemperatureSeriesAnalysisTest {
     @Test
     public void testAverage() {
         double[] temperatureSeries = {3.0, -5.0, 1.0, 5.0};
+        double[] addToSeries = {7.8, 145,3, -4,7};
         TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
-        double expResult = 1.0;
-
-        double actualResult = seriesAnalysis.average();
+        double expAver = 1.0;
+        double expDevit = 14.0;
+        double actualAver = seriesAnalysis.average();
         double devit = seriesAnalysis.deviation();
         double closest = seriesAnalysis.findTempClosestToZero();
-        System.out.println(actualResult);
+        System.out.println(actualAver);
         System.out.println(devit);
+        System.out.println(seriesAnalysis.min());
+        System.out.println(seriesAnalysis.max());
         System.out.println(closest);
-        double closest3 = seriesAnalysis.findTempClosestToValue((double)2);
-        System.out.println(closest3);
-        assertEquals(expResult, actualResult, 0.00001);
+        double closest2 = seriesAnalysis.findTempClosestToValue((double)2);
+        System.out.println(closest2);
+        System.out.println(Arrays.toString(seriesAnalysis.findTempsLessThen((double) 3)));
+        System.out.println(Arrays.toString(seriesAnalysis.findTempsGreaterThen(((double) 2))));
+        assertEquals(expAver, actualAver, 0.00001);
+        seriesAnalysis.addTemps(7.8, 145,3, -4,7);
+        System.out.println(Arrays.toString(seriesAnalysis.temperatureSeries));
+        TempSummaryStatistics summary = seriesAnalysis.summaryStatistics();
+        System.out.println(summary.avgTemp);
+
     }
     
 
